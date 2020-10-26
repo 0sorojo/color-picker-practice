0080c7840c0f3ff4924474c39d6936111f582bb2
0sorojo <redrootproductions@gmail.com>
2020-10-26 13:53:03 -0400

    made it a disco color selector

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import SingleColor from './SingleColor';
+import RgbToHex from './utils/RgbToHex';
 
 import Values from 'values.js';
 
 const App = () => {
-  const [color, setColor] = useState('');
+  const [color, setColor] = useState('#f09090');
   const [error, setError] = useState(false);
   const [list, setList] = useState(new Values('#f09090').all(10));
 
@@ -12,6 +13,29 @@ const App = () => {
     e.preventDefault();
     try {
       let colors = new Values(color).all(10);
+      console.log(colors);
+      setList(colors);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
+  };
+
+  const discoSelector = () => {
+    let r = Math.floor(Math.random() * 256) + 1;
+    let g = Math.floor(Math.random() * 256) + 1;
+    let b = Math.floor(Math.random() * 256) + 1;
+
+    let randomColor = [r, g, b];
+
+    console.log(randomColor);
+
+    const randomHex = RgbToHex(...randomColor);
+    setColor(randomHex);
+    console.log(color);
+    try {
+      let colors = new Values(color).all(10);
+      console.log(colors);
       setList(colors);
     } catch (error) {
       setError(true);
@@ -34,6 +58,9 @@ const App = () => {
           />
           <button className='btn' type='submit'>
             submit
+          </button>
+          <button className='btn' onClick={discoSelector}>
+            disco
           </button>
         </form>
       </section>
